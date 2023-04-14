5cc6730da3398e4a9ef2d74d7990564fc594f06c
xdw <3331732340@qq.com>
2023-04-14 21:02:59 +0800

    add: Hud Offset

--- src/client/kotlin/cn/xdw/ColorWheelHud.kt
+++ src/client/kotlin/cn/xdw/ColorWheelHud.kt
@@ -2,20 +2,26 @@ package cn.xdw
 
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.util.InputUtil
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.item.ItemStack
 import net.minecraft.item.Items
+import org.lwjgl.glfw.GLFW
 
 
 class ColorWheelHud:HudRenderCallback {
+    companion object{
+        var curIndex = 8
+    }
     override fun onHudRender(matrixStack: MatrixStack?, tickDelta: Float) {
         val client = MinecraftClient.getInstance()
+        if(!InputUtil.isKeyPressed(client.window.handle, GLFW.GLFW_KEY_LEFT_ALT)) return
+
         val x = client.window.scaledWidth
         val y = client.window.scaledHeight
         matrixStack?.push()
         val itemRenderer = MinecraftClient.getInstance().itemRenderer
         val items = List(16){ItemStack(Items.APPLE,64)}
-        val curIndex = 8
         items.forEachIndexed { index, item ->
             val i = 16 * (index - curIndex) + x / 2 - 8
             val j = 16*2+y/2-8
--- src/client/kotlin/cn/xdw/MouseHandle.kt
+++ src/client/kotlin/cn/xdw/MouseHandle.kt
@@ -7,12 +7,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 class MouseHandle {
     companion object{
+        @Suppress("UNUSED_PARAMETER")
         fun mouseHandle(window: Long, horizontal: Double, vertical: Double, info: CallbackInfo) {
             val client = MinecraftClient.getInstance()
             if (client.currentScreen != null) return
             InputUtil.isKeyPressed(client.window.handle, GLFW.GLFW_KEY_LEFT_ALT).takeIf { it }?.let {
                 info.cancel()
-
+                ColorWheelHud.curIndex = (ColorWheelHud.curIndex+vertical.toInt()).coerceIn(0,16)
             }
             val a = 1
         }
