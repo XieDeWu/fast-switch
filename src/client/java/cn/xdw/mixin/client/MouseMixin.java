6ddeb5de08ff7a03f3573b6a356985679b2969e1
xdw <3331732340@qq.com>
2023-04-14 19:53:32 +0800

    add: MouseMixin

---
+++ src/client/java/cn/xdw/mixin/client/MouseMixin.java
@@ -0,0 +1,17 @@
+package cn.xdw.mixin.client;
+
+import cn.xdw.MouseHandle;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.Mouse;
+
+@Mixin(Mouse.class)
+public class MouseMixin {
+    @Inject(method = "onMouseScroll", at = @At("HEAD"), cancellable = true)
+    public void onMouseScroll(long window, double horizontal, double vertical, CallbackInfo info) {
+        MouseHandle.Companion.mouseHandle(window, horizontal, vertical, info);
+    }
+}
---
+++ src/client/kotlin/cn/xdw/MouseHandle.kt
@@ -0,0 +1,20 @@
+package cn.xdw
+
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.util.InputUtil
+import org.lwjgl.glfw.GLFW
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+
+class MouseHandle {
+    companion object{
+        fun mouseHandle(window: Long, horizontal: Double, vertical: Double, info: CallbackInfo) {
+            val client = MinecraftClient.getInstance()
+            if (client.currentScreen != null) return
+            InputUtil.isKeyPressed(client.window.handle, GLFW.GLFW_KEY_LEFT_ALT).takeIf { it }?.let {
+                info.cancel()
+
+            }
+            val a = 1
+        }
+    }
+}
