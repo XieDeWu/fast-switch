b78cfacb76fd80e7f3b3d6fadf019b3117e9b5bc
xdw <3331732340@qq.com>
2023-04-21 23:44:05 +0800

    add BlockPlaceCheck

--- src/client/kotlin/cn/xdw/handle/PlayerHandle.kt
+++ src/client/kotlin/cn/xdw/handle/PlayerHandle.kt
@@ -2,30 +2,37 @@ package cn.xdw.handle
 
 import cn.xdw.data.HudData
 import net.fabricmc.fabric.api.event.player.UseBlockCallback
-import net.minecraft.item.BlockItem
-import net.minecraft.item.ItemPlacementContext
-import net.minecraft.item.ItemUsageContext
+import net.minecraft.client.MinecraftClient
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.util.math.Vec3d
 
 class PlayerHandle {
     companion object{
+        @Suppress("NestedLambdaShadowedImplicitParameter")
         fun register(){
-            UseBlockCallback.EVENT.register(UseBlockCallback { player, world, hand, hitResult ->
-                if(world.isClient
-                    && HudData.currentItemGroup.switchDisplay(false)
-                    && hand == Hand.MAIN_HAND){
-                    val itemStack = player.mainHandStack
-                    (itemStack.item as? BlockItem)
-                        ?.block
-                        ?.getPlacementState(ItemPlacementContext(ItemUsageContext(world, player, hand, itemStack, hitResult)))
-                        ?.canPlaceAt(world,hitResult.blockPos.offset(hitResult.side))
-                        ?.let {
-                            HudData.currentItemGroup.perlinNextItem()
-                            return@UseBlockCallback ActionResult.SUCCESS
+            UseBlockCallback.EVENT.register( run {
+                var oldHitResult = BlockHitResult(Vec3d.ZERO,Direction.UP, BlockPos.ORIGIN,false)
+                UseBlockCallback { _, world, hand, hitResult ->
+                    if(world.isClient
+                        && HudData.currentItemGroup.switchDisplay(false)
+                        && (hand == Hand.MAIN_HAND)
+                        && (hitResult != oldHitResult)
+                    ){
+                        oldHitResult = hitResult
+                        val client = MinecraftClient.getInstance()
+                        client.interactionManager?.let {
+                            it.interactBlock(client.player, hand, oldHitResult).takeIf { it.isAccepted }?.let {
+                                HudData.currentItemGroup.perlinNextItem()
+                                return@UseBlockCallback ActionResult.SUCCESS
+                            }
                         }
+                    }
+                    ActionResult.PASS
                 }
-                ActionResult.PASS
             })
         }
     }
