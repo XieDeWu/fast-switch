92dc698becfc7cb22fda6b639c497d60525d1d6d
xdw <3331732340@qq.com>
2023-04-25 21:46:59 +0800

    optimized code

--- src/client/kotlin/cn/xdw/handle/CommandHandle.kt
+++ src/client/kotlin/cn/xdw/handle/CommandHandle.kt
@@ -1,29 +1,36 @@
 package cn.xdw.handle
 
+import com.mojang.brigadier.Command
 import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.*
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
+import net.minecraft.client.network.ClientCommandSource
 import net.minecraft.text.Text
 
 
 class CommandHandle {
     companion object{
-        fun registry(){
-            ClientCommandRegistrationCallback.EVENT.register{ _, _ ->
-                getActiveDispatcher()?.register(
-                    literal("fast-switch")
-                        .then(literal("add")
-                            .then(argument("customGroupName",StringArgumentType.greedyString()).executes {
-
-                                it.source.sendFeedback(Text.literal("已添加自定义标签组:${it.input}"))
-                                0
-                            }))
-                        .then(literal("del")
-                            .then(argument("customGroupName",StringArgumentType.greedyString()).executes {
-                                it.source.sendFeedback(Text.literal("已删除自定义标签组:${it.input}"))
-                                0
-                            }))
-                )
+        fun registry() {
+            ClientCommandRegistrationCallback.EVENT.register { _, _ ->
+                getActiveDispatcher()?.register(run {
+                    val add = "add"
+                    val del = "del"
+                    val handle: (String) -> (CommandContext<FabricClientCommandSource>) -> Int = { opt -> code@{ pack ->
+                            val name = pack.nodes.last().range.get(pack.input)
+                                .takeIf { "^[a-z_]*$".toRegex().matches(it) } ?: run {
+                                    pack.source.sendFeedback(Text.literal("自定义标签组名应仅包含小写字母与下划线!"))
+                                    return@code -1
+                                }
+                            pack.source.sendFeedback(Text.literal("已${when(opt){add->"增加";del->"删除";else->""}}自定义标签组:${name}"))
+                            0
+                        }
+                    }
+                    listOf(add,del).fold(literal("fast-switch")){acc, s ->
+                        acc.then(literal(s).then(argument("customGroupName", StringArgumentType.greedyString()).executes(handle(s))))
+                    }
+                })
             }
         }
     }
